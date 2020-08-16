b95c5600517a2b5454ce60c9599deedde88c48a3
Radosław Górski <radoslawgorski8@gmail.com>
2020-08-16 13:15:27 +0200

    Revert "Zmodyfikowana wersja"
    This reverts commit 7fc9647391501e301e7223262222b9f5b85eee18.

--- js/backstage.js
+++ js/backstage.js
@@ -1,12 +1,90 @@
 // --- Wczytaj dźwięki
-
+var badAnswer = new Audio("sound/badAnswer.mp3");
 var goodAnswer = new Audio("sound/goodAnswer.mp3");
 
 // --- zarezerwuj potrzebne zmienne
-var code,start,end,time,i=0,row=1;
+var code,start,end,time,go=false,i=0,row=1;
 var highscores = [];
 
+function setChar() {
+    var n = Math.floor(Math.random()*26);
+        if(n==0) {
+            code = code + 'q';
+        } else if(n==1) {
+            code = code + 'w';
+        } else if(n==2) {
+            code = code + 'e';
+        } else if(n==3) {
+            code = code + 'r';
+        } else if(n==4) {
+            code = code + 't';
+        } else if(n==5) {
+            code = code + 'y';
+        } else if(n==6) {
+            code = code + 'u';
+        } else if(n==7) {
+            code = code + 'i';
+        } else if(n==8) {
+            code = code + 'o';
+        } else if(n==9) {
+            code = code + 'p';
+        } else if(n==10) {
+            code = code + 'a';
+        } else if(n==11) {
+            code = code + 's';
+        } else if(n==12) {
+            code = code + 'd';
+        } else if(n==13) {
+            code = code + 'f';
+        } else if(n==14) {
+            code = code + 'g';
+        } else if(n==15) {
+            code = code + 'h';
+        } else if(n==16) {
+            code = code + 'j';
+        } else if(n==17) {
+            code = code + 'k';
+        } else if(n==18) {
+            code = code + 'l';
+        } else if(n==19) {
+            code = code + 'z';
+        } else if(n==20) {
+            code = code + 'x';
+        } else if(n==21) {
+            code = code + 'c';
+        } else if(n==22) {
+            code = code + 'v';
+        } else if(n==23) {
+            code = code + 'b';
+        } else if(n==24) {
+            code = code + 'n';
+        } else if(n==25) {
+            code = code + 'm';
+        }
+}
+function ready() {
+    if(sec<=0) {
+        return true;
+    } else {
+        return false;
+    };
+}
 
+function answerIsTrue() {
+    if(ans.value==code) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function displayWrongAnswer() {
+    false_answer.style.display = 'block';
+    setTimeout(function(){false_answer.style.display = 'none';},2000);
+}
+function calculateTime() {
+    end = new Date().getTime();
+    time = (end - start)/1000;
+}
 function placeInHighScore(time) {
     highscores[i] = time;
     i++;
--- js/sketch.js
+++ js/sketch.js
@@ -1,64 +1,64 @@
+var sec = 3; 
 var znakow_w_kodzie = 4;
-var gracz="";
 
 // -- łatwy dostęp do elementów DOM
-var start_button = document.querySelector("#start-button");
-var menu_button = document.querySelector("#menu-button");
-var change_button = document.querySelector("#changeName-button");
+var counting = document.querySelector('#counting').style;
+var start_button = document.querySelector('#start-button');
 var seconds_to_start = document.querySelector("#seconds-to-start");
 var codeP = document.querySelector("#code");
 var ans = document.querySelector('#answer');
+var false_answer = document.querySelector('#false-answer');
 var seconds_to_answer = document.querySelector('#seconds-to-answer');
 var summarize = document.querySelector('#summarize');
 
 
 // --- Ustaw zdarzenia
 start_button.addEventListener("click",count);
-menu_button.addEventListener("click",showGame);
-ans.addEventListener("input",check);
-change_button.addEventListener("click",changeName);
-function changeName() {
-  document.getElementsByClassName("game-wrapper")[0].style.display = "none";
-  document.getElementsByClassName("menu")[0].style.display = "block";
-}
-function showGame() {
-  document.getElementsByClassName("game-wrapper")[0].style.display = "block";
-  document.getElementsByClassName("menu")[0].style.display = "none";
-  gracz = document.getElementById("name-input").value;
-  document.getElementById("name").innerHTML = gracz;
-}
+ans.addEventListener("change",check)
+
+
+
+
+// ---------------  ALGORYTMY ----------------------//
+
 
 //------------ Kiedy klikniesz w przycisk 'Zacznij'
 function count() {
+    counting.display = "block";
     summarize.style.display = "none";
-    start_button.style.display = "none";
+    start_button.style.display = "none"; 
+    ans.style.display = "block";
+    
     ans.value = "";
-    var sec = 3;
-    seconds_to_start.innerHTML = sec;
+    
     setCode();
-
+    sec = 3;
+    seconds_to_start.innerHTML = sec;
+    go = true;
+    
+    
     // odlicza czas i pokazuje kod
     var counter = setInterval(function() {
-      sec--;
-      if(sec>0) {
-          seconds_to_start.innerHTML = sec;
-      } else {
-          seconds_to_start.innerHTML = "";
-          ans.style.display = "block";
-          document.getElementById("game").style.display="block";
-          ans.focus();
-          codeP.style.display = "block";
-          start = new Date().getTime();
-          clearInterval(counter);
-      }
+        if(go) {
+            sec--;
+            if(!ready()) {
+                seconds_to_start.innerHTML = sec;  
+            } else {
+                counting.display = "none";
+                codeP.style.display = "block";
+                start = new Date().getTime();
+                go = false;
+                clearInterval(counter);
+            }
+        }
     },1000)
 }
 
 //------------- Kiedy wpiszesz kod
 function check() {
-    if(ans.value==code || ans.value==code.toLowerCase()) {
-        end = new Date().getTime();
-        time = (end - start)/1000;
+ 
+    if(answerIsTrue()) {
+        calculateTime();
         placeInHighScore(time);
         seconds_to_answer.innerHTML = time;
         summarize.style.display = "block";
@@ -67,6 +67,9 @@ function check() {
         ans.style.display = "none";
         goodAnswer.play();
         addRow('ranking');
+    } else {
+        displayWrongAnswer();
+        badAnswer.play();
     }
 }
 
@@ -80,21 +83,25 @@ function addRow(tableID) {
     // Append a text node to the cell
     var newText = document.createTextNode(row+'.');
     newCell.appendChild(newText);
-
+    
     //----- Insert a cell in the row at index 1
     var newCell = newRow.insertCell(1);
     // Append a text node to the cell
     var newText = document.createTextNode(time);
     newCell.appendChild(newText);
-
+    
+    
     row++;
 }
 
 function setCode() {
     code = "";
+    
     for(var i = 0 ; i<znakow_w_kodzie ; i++) {
-      var charCode = Math.floor(Math.random()*26) + 65;
-        code +=String.fromCharCode(charCode);
+        setChar();
     }
     codeP.innerHTML = code;
 }
+
+
+
